92e05972ee6b6019ac2df4e9b3ad3e931537c138
nafees nazik <nafeesnazik21@gmail.com>
2023-09-20 08:11:23 +0530

    feat: init app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import AnimatePlugin from "tailwindcss-animate";
+import WindyRadixPalette from "windy-radix-palette";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+  theme: {},
+  plugins: [AnimatePlugin, WindyRadixPalette],
+};
+export default config;

c1027c36f2757b3cbe3d2468d09efcfa3e40822e
nafees nazik <nafeesnazik21@gmail.com>
2023-09-21 07:20:05 +0530

    feat: add shad cn ui

--- src/app/(unauthenticated)/sign-in/page.tsx
+++ src/app/(unauthenticated)/sign-in/page.tsx
@@ -1,3 +1,13 @@
-export default function SignInPage() {
-  return <div>sign in</div>;
+import { Input } from "@/components/ui/input";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center">
+      <div className=" sm:rounded-2xl  max-w-md">
+        <div className="px-4 py-8 sm:px-16">
+          <Input />
+        </div>
+      </div>
+    </div>
+  );
 }

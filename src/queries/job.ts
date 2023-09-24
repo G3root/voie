df2210f5f1adf40325782ad89a19a48e6b0bba5a
nafees nazik <nafeesnazik21@gmail.com>
2023-09-24 23:24:33 +0530

    fix: url

--- src/queries/job.ts
+++ src/queries/job.ts
@@ -1,7 +1,7 @@
 import { TJobGetApiResp } from "@/app/api/job/route";
 
 export const getJobs = async () => {
-  const req = await fetch("/api/job");
+  const req = await fetch("http://localhost:3000/api/job");
   const res = (await req.json()) as TJobGetApiResp;
 
   return res;

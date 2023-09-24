1c62e0cfc1625209bfaa20450c04e5b6e5084e9f
nafees nazik <nafeesnazik21@gmail.com>
2023-09-24 08:35:06 +0530

    feat: add job create route

---
+++ src/app/api/job/route.ts
@@ -0,0 +1,33 @@
+import { InferApiRoute, api } from "@/lib/api";
+import { jobs } from "@/schema";
+import { jobCreateFormSchema, jobGetSchema } from "@/zod-schemas/job";
+import { eq } from "drizzle-orm";
+
+export const POST = (req: Request) =>
+  api
+    .create(req)
+    .input({ body: jobCreateFormSchema })
+    .procedure(async ({ ctx, input }) => {
+      return ctx.db.insert(jobs).values(input.body);
+    });
+
+export const GET = (req: Request) =>
+  api
+    .create(req)
+    .input({ query: jobGetSchema })
+    .procedure(async ({ ctx, input }) => {
+      const {
+        query: { id: jobId },
+      } = input;
+      const query = await ctx.db.query.jobs.findFirst({
+        where: eq(jobs.publicId, jobId),
+      });
+
+      if (!query) {
+        throw new Error(`job with the id:${jobId} not found`);
+      }
+      return query;
+    });
+
+export type TPostApiResponse = InferApiRoute<typeof POST>;
+export type TGetApiResponse = InferApiRoute<typeof GET>;

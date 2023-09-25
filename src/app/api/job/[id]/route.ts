eeaa0c4cbaa4d17682a7f6a06e29874522c76e77
nafees nazik <nafeesnazik21@gmail.com>
2023-09-25 13:37:19 +0530

    feat: add delete job endpoint

--- src/app/api/job/[id]/route.ts
+++ src/app/api/job/[id]/route.ts
@@ -1,6 +1,8 @@
 import { InferApiRoute, api } from "@/lib/api";
+import { AllJobsCacheTag } from "@/lib/cache-tags";
 import { jobs } from "@/schema";
 import { eq } from "drizzle-orm";
+import { revalidateTag } from "next/cache";
 import * as z from "zod";
 
 export const GET = (req: Request, { params }: { params: { id: string } }) =>
@@ -18,6 +20,30 @@ export const GET = (req: Request, { params }: { params: { id: string } }) =>
         throw new Error(`job with the id: ${paramId} not found`);
       }
       return query;
+    });
+
+export const DELETE = (req: Request, { params }: { params: { id: string } }) =>
+  api
+    .create(req)
+    .input(undefined)
+    .procedure(async ({ ctx }) => {
+      const paramId = z.string().min(1).parse(params.id);
+
+      const query = await ctx.db.query.jobs.findFirst({
+        where: eq(jobs.publicId, paramId),
+      });
+
+      if (!query) {
+        throw new Error(`job with the id: ${paramId} not found`);
+      }
+      const deleted = await ctx.db
+        .delete(jobs)
+        .where(eq(jobs.publicId, paramId))
+        .returning();
+
+      revalidateTag(AllJobsCacheTag);
+
+      return deleted;
     });
 
 export type TJobGetOneApiRes = InferApiRoute<typeof GET>;
--- src/queries/job.ts
+++ src/queries/job.ts
@@ -1,9 +1,10 @@
 import { TJobGetOneApiRes } from "@/app/api/job/[id]/route";
-import { TJobGetApiResp, PostCacheTag } from "@/app/api/job/route";
+import { TJobGetApiResp } from "@/app/api/job/route";
+import { AllJobsCacheTag } from "@/lib/cache-tags";
 
 export const getJobs = async () => {
   const req = await fetch("http://localhost:3000/api/job", {
-    next: { tags: [PostCacheTag] },
+    next: { tags: [AllJobsCacheTag] },
   });
   const res = (await req.json()) as TJobGetApiResp;
 
@@ -13,6 +14,15 @@ export const getJobs = async () => {
 export const getJobById = async (id: string) => {
   const req = await fetch(`http://localhost:3000/api/job/${id}`);
   const res = (await req.json()) as TJobGetOneApiRes;
+
+  return res;
+};
+
+export const deleteJobById = async (id: string) => {
+  const req = await fetch(`http://localhost:3000/api/job/${id}`, {
+    method: "DELETE",
+  });
+  const res = await req.json();
 
   return res;
 };

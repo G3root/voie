656dc04133146db226d497b32adfe0ff0d5a9a36
nafees nazik <nafeesnazik21@gmail.com>
2023-09-25 15:09:50 +0530

    feat: add job list table

---
+++ src/app/(authenticated)/admin/job/(list)/components/job-list-table.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import { TJob } from "@/zod-schemas/job";
+import { MoreHorizontal } from "lucide-react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { deleteJobById } from "@/queries/job";
+import { toast } from "sonner";
+
+const columns: ColumnDef<TJob>[] = [
+  {
+    accessorKey: "id",
+    header: "id",
+  },
+  {
+    accessorKey: "title",
+    header: "Position",
+  },
+  {
+    accessorKey: "level",
+    header: "Level",
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+  },
+  {
+    accessorKey: "locationType",
+    header: "Location Type",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const job = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={async () => {
+                try {
+                  await deleteJobById(job.publicId);
+                  toast.success(`successfully deleted: ${job.title}`);
+                } catch (error) {
+                  toast.error(`failed to delete: ${job.title}`);
+                }
+              }}
+            >
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
+interface JobListTableProps {
+  data: TJob[];
+}
+
+export const JobListTable: React.FC<JobListTableProps> = (props) => {
+  const { data } = props;
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};

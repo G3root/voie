f8a8d1d2a296bbdb00086edf123c446565d448d6
nafees nazik <nafeesnazik21@gmail.com>
2023-09-25 15:10:03 +0530

    feat: render job list table

--- src/app/(authenticated)/admin/job/(list)/page.tsx
+++ src/app/(authenticated)/admin/job/(list)/page.tsx
@@ -1,12 +1,27 @@
+import { getJobs } from "@/queries/job";
 import { PageLayout } from "../../components/page-layout";
+import { JobListTable } from "./components/job-list-table";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
-export default function JobListPage() {
+export default async function JobListPage() {
+  const jobList = await getJobs();
   return (
     <PageLayout
       title="Jobs"
       description="Here is a list of your current job postings"
+      actions={[
+        {
+          id: "create-job",
+          element: (
+            <Link className={buttonVariants()} href="/admin/job/create">
+              create job
+            </Link>
+          ),
+        },
+      ]}
     >
-      hello
+      {jobList.success ? <JobListTable data={jobList.data} /> : null}
     </PageLayout>
   );
 }

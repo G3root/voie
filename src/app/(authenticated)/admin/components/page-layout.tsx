407fc979ac0192e42083cee90a936b55ff85e0cd
nafees nazik <nafeesnazik21@gmail.com>
2023-09-25 13:37:54 +0530

    feat: add actions prop

--- src/app/(authenticated)/admin/components/page-layout.tsx
+++ src/app/(authenticated)/admin/components/page-layout.tsx
@@ -1,15 +1,26 @@
 import { PageHeader, PageHeaderProps } from "@/components/common/page-header";
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 interface PageLayoutProps extends PageHeaderProps {
   children?: ReactNode;
+  actions?: { id: string; element: ReactNode }[];
 }
 
 export const PageLayout: React.FC<PageLayoutProps> = (props) => {
-  const { title, description, children } = props;
+  const { title, description, children, actions } = props;
   return (
     <div>
-      <PageHeader title={title} description={description} />
+      <div className="flex items-center justify-between">
+        <PageHeader title={title} description={description} />
+
+        <div className="flex gap-x-2">
+          {actions
+            ? actions.map((item) => (
+                <Fragment key={item.id}>{item.element}</Fragment>
+              ))
+            : null}
+        </div>
+      </div>
       {children}
     </div>
   );
